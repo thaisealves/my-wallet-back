355570e6c933ba75602455ec407a3d4b6eaaeaeb
thaisealves <alves.thaisesilva@gmail.com>
2022-07-04 07:56:36 -0300

    fix: changing transaction schema

--- src/middlewares/transactionValidationMiddleware.js
+++ src/middlewares/transactionValidationMiddleware.js
@@ -4,7 +4,7 @@ export function transactionValidationMiddleware (req, res, next){
     const validation = transactionSchema.validate(req.body);
   if (validation.error) {
     console.log(validation.error)
-    return res.sendStatus(422);
+    return res.status(422).send(validation.error);
   }
   next();
 }
--- src/schemas/transactionSchema.js
+++ src/schemas/transactionSchema.js
@@ -4,7 +4,7 @@ const transactionSchema = joi.object({
   description: joi.string().required(),
   value: joi
     .string()
-    .pattern(/^(0|[1-9][0-9]{0,2})(\.\d{3})*(\,\d{1,2})?$/)
+    .pattern(/^(0|[1-9][0-9]{0,2})(\.\d{3}|\,\d{3})*(\,\d{1,2}|\.\d{1,2})?$/)
     .required(),
   status: joi.string().valid("inflow", "outflow").required(),
 });
