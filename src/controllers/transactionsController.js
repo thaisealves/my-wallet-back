91ba71439adf2b660041b2f9ee8cd5fff59e08d3
thaisealves <alves.thaisesilva@gmail.com>
2022-07-04 02:28:23 -0300

    fix: adding day to the transaction

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -1,10 +1,13 @@
 import { db, objectId } from "../dbConection/mongo.js";
+import dayjs from "dayjs";
 export async function postTransactions(req, res) {
+  const now = dayjs();
+
   const { user } = res.locals;
   const userId = new objectId(user._id);
 
   try {
-    await db.collection("transactions").insertOne({...req.body, userId});
+    await db.collection("transactions").insertOne({...req.body, userId, day: now.format("DD/MM")});
 
     res.sendStatus(201);
   } catch (error) {
